0cea3f41c94637f2640a61ed4f40ab6473e5c39a
ZHX1-java <139690948+ZHX1-java@users.noreply.github.com>
2023-08-25 17:21:59 +0800

    Add files via upload

---
+++ Day13-oop-extendsDemo/src/a01oopExtendsDemo10/Test.java
@@ -0,0 +1,29 @@
+package ClsaaTest;
+
+public class Test {
+    public static void main(String[] args) {
+        //创建讲师对象
+        Lecturer l = new Lecturer("z01", "zhangsan");
+        l.work();
+        System.out.println(l.getId() + "," + l.getName());
+        System.out.println("***********************************");
+
+        //创建助教对象
+        Tutor t = new Tutor("z02", "lisi");
+        t.work();
+        System.out.println(t.getId() + "," + t.getName());
+        System.out.println("***********************************");
+
+        //创建维护专员对象
+        Maintainer m = new Maintainer("z03", "wangwu");
+        m.work();
+        System.out.println(m.getId() + "," + m.getName());
+        System.out.println("***********************************");
+
+        //创建采购员对象
+        Buyer b = new Buyer("z04", "zhaoliu");
+        b.work();
+        System.out.println(b.getId() + "," + b.getName());
+
+    }
+}
